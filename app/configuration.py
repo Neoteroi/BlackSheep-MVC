753973a6339f234da95dd2b44ea7d86c0b4160d1
Roberto Prevato <roberto.prevato@gmail.com>
2020-09-19 21:57:26 +0200

    two small improvements

--- app/configuration.py
+++ app/configuration.py
@@ -4,11 +4,10 @@ from roconfiguration import Configuration
 def load_configuration() -> Configuration:
     configuration = Configuration()
 
-    # NB: loads settings from a yaml file, then supports overriding by environment
-    # variables;
+    # NB: loads settings from a yaml file, then environment variables
     # refer to https://github.com/RobertoPrevato/roconfiguration documentation
     # for more details.
     configuration.add_yaml_file("settings.yaml")
-    configuration.add_environmental_variables("BLACKSHEEP_")
+    configuration.add_environmental_variables()
 
     return configuration
--- server.py
+++ server.py
@@ -3,11 +3,16 @@
 To run with uvicorn cli, with hot reload:
     $ uvicorn server:app --reload --log-level info
 """
-import uvloop
+try:
+    import uvloop
+except ModuleNotFoundError:
+    print("[*] Running without `uvloop`")
+    uvloop = ...
 from app.program import configure_application
 from app.services import configure_services
 from app.configuration import load_configuration
 
-uvloop.install()
+if uvloop is not ...:
+    uvloop.install()
 
 app = configure_application(*configure_services(load_configuration()))
